c495e1d1aeb3b53c62272d11d59a02504900b161
jamie <569309786@qq.com>
2017-02-19 10:38:22 +0800

    1

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MACHENIKE on 2017/2/19.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+console.log(React)
+
+ReactDom.render(
+   <div>123</div>,
+   document.querySelector('#app')
+)
